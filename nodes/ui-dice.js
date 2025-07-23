cdf0abe31b72de59134d92972c5b11a2d1a6f8f1
yoshoku <yoshoku@outlook.com>
2025-07-23 16:23:54 +0900

    refactor: remove unnecessary methods

--- nodes/ui-dice.js
+++ nodes/ui-dice.js
@@ -11,34 +11,11 @@ module.exports = function (RED) {
 
         // server-side event handlers
         const evts = {
-            onAction: true,
-            beforeSend: function (msg) {
-                // check for any dynamic properties being set
-                const updates = msg.ui_update
-                if (updates) {
-                    if (typeof updates.example !== 'undefined') {
-                        // save the "example" property in the Node-RED statestore
-                        base.stores.state.set(base, node, msg, 'example', updates.example)
-                    }
-                }
-                return msg
-            },
             onInput: function (msg, send, done) {
                 // store the latest value in our Node-RED datastore
                 base.stores.data.save(base, node, msg)
                 // send it to any connected nodes in Node-RED
                 send(msg)
-            },
-            onSocket: {
-                'my-custom-event': function (conn, id, msg) {
-                    console.info('"my-custom-event" received:', conn.id, id, msg)
-                    console.info('conn.id:', conn.id)
-                    console.info('id:', id)
-                    console.info('msg:', msg)
-                    console.info('node.id:', node.id)
-                    // emit a msg in Node-RED from this node
-                    node.send(msg)
-                }
             }
         }
 
